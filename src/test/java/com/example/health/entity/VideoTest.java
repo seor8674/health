961128529370690dd2dc2ece25423eb683f66b11
hwanjun <hwanjuni70@gmail.com>
2021-07-21 22:06:39 +0900

    last commit

--- src/test/java/com/example/health/entity/VideoTest.java
+++ src/test/java/com/example/health/entity/VideoTest.java
@@ -14,25 +14,7 @@ class VideoTest {
     @Autowired
     VideoRepository videoRepository;
 
-    @Test
-    public void test1(){
-        Video qwe = new Video("qwe","rqr");
-        Video qwe2 = new Video("qwerqwed","qrqqq");
-        System.out.println("qwe2 = " + qwe2.getName());
-        System.out.println("qwe2 = " + qwe2.getId());
-        System.out.println("qwe = " + qwe.getName());
-        System.out.println("qwe = " + qwe.getId());
-        Video save = videoRepository.save(qwe);
-        Video save1 = videoRepository.save(qwe2);
-        Video byVideoid = videoRepository.findByName(qwe.getName());
-        Video byVideoid1 = videoRepository.findByName(qwe2.getName());
-        org.assertj.core.api.Assertions.assertThat(qwe.getName()).isEqualTo(byVideoid.getName());
-        org.assertj.core.api.Assertions.assertThat(qwe2.getName()).isEqualTo(byVideoid1.getName());
-    }
-    @Test
-    public void test2(){
-        Video qwe = videoRepository.findByName("qwe");
-        org.assertj.core.api.Assertions.assertThat(qwe).isNull();
-    }
+
+
 
 }
--- src/test/java/com/example/health/repository/UserRepositoryTest.java
+++ src/test/java/com/example/health/repository/UserRepositoryTest.java
@@ -20,38 +20,6 @@ class UserRepositoryTest {
     @Autowired
     VideoItemRepository videoItemRepository;
 
-    @Test
-    public void test1(){
 
-        User user = new User("이환준","seor75","1234","123@123");
-        User user2 = new User("김렁쟈","seor7545","1234","123@123");
-        Video video = new Video("video1","title1");
-        Video video2 = new Video("video2","title2");
-        userRepository.save(user2);
-        userRepository.save(user);
-        videoRepository.save(video);
-        videoRepository.save(video2);
-        User byNickname = userRepository.findByNickname(user.getNickname());
-        byNickname.addvideoitem(new VideoItem(video));
-        System.out.println(byNickname.getVideoItemList().size());
-
-
-    }
-    @Test
-    public void test2(){
-        User user = new User("이환준","seor75","1234","123@123");
-        User user2 = new User("김렁쟈","seor7545","1234","123@123");
-        Video video = new Video("video1","title1");
-        Video video2 = new Video("video2","title2");
-        userRepository.save(user2);
-        userRepository.save(user);
-        videoRepository.save(video);
-        videoRepository.save(video2);
-        VideoItem videoItem = new VideoItem(videoRepository.findByName(video.getName()));
-        user.addvideoitem(videoItem);
-        videoItemRepository.save(videoItem);
-        User byNicknamefetch = userRepository.findByNicknamefetch(user.getNickname());
-        System.out.println(byNicknamefetch.getVideoItemList().size());
-    }
 
 }
